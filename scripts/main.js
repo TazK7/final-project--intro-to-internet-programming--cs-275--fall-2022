f43dc2b1902e9278904ca2a1e8aa29e1476a01d2
TazK7 <khantazz07@gmail.com>
2022-12-10 14:40:16 -0500

    Added Event Listener to show/remove Modal
    Added JS to display/remove the modal and to also remove scrolling when modal is present per the assignment instructions.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 window.onload = () => {
     let hide = document.querySelectorAll(`ul`)[0];
+    let selectDiv = document.querySelectorAll(`div`)[0];
+    let displayModal = document.getElementById(`cover`);
     let card = document.getElementById(`card`);
+    let selectBody = document.querySelector(`body`);
 
+    //This EventListener is displaying and removing the menu when the show menu button is clicked
     card.addEventListener(`click`, () => {
         if(!hide.classList.contains(`hide`)) {
             hide.classList.add(`hide`);
@@ -10,5 +14,16 @@ window.onload = () => {
             hide.classList.remove(`hide`);
         }
     });
-};
 
+    //Adds Modal when you click Show Modal and removes it when you click the background layer
+    displayModal.addEventListener(`click`, () => {
+        if(selectDiv.classList.contains(`hide`)) {
+            selectDiv.classList.remove(`hide`);
+            selectBody.classList.add(`remove-scroll`);
+        } else{
+            !selectDiv.classList.contains(`hide`);
+            selectDiv.classList.add(`hide`);
+            selectBody.classList.remove(`remove-scroll`);
+        }
+    });
+};
